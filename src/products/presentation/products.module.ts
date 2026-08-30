c383a8c5a0a951d6e8bb141be47060a6d3bc2ff3
Ayush Bommana <bommanaayush07@gmail.com>
2026-08-31 00:09:26 +0530

    fix: import drizzle module inside products module

--- src/products/presentation/products.module.ts
+++ src/products/presentation/products.module.ts
@@ -4,9 +4,11 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { ProductsService } from './services/products.service';
 import { PRODUCT_REPOSITORY_TOKEN } from '../application/ports/product.repository.constants';
 import DrizzleProductRepository from '../intrastructure/adapters/drizzle-product.repository';
+import { CreateProductHandler } from '../application/use-cases/create-product/create-product.handler';
+import { DrizzleModule } from '../../shared/infrastructure/database/drizzle/drizzle.module';
 
 @Module({
-  imports: [CqrsModule],
+  imports: [CqrsModule, DrizzleModule],
   controllers: [ProductsController],
   providers: [
     ProductsService,
@@ -14,6 +16,7 @@ import DrizzleProductRepository from '../intrastructure/adapters/drizzle-product
       provide: PRODUCT_REPOSITORY_TOKEN,
       useClass: DrizzleProductRepository,
     },
+    CreateProductHandler,
   ],
 })
 export class ProductsModule {}
